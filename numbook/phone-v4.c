5dc74b33337ad93b7a14c3e9071857e3be076991
zeroet <y36609672@gmail.com>
2020-12-16 23:58:39 +0100

    gg

--- numbook/phone-v4.c
+++ numbook/phone-v4.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define INIT_CAPCITY 100
-
+#define BUF_SIZE 100
 typedef struct t_list
 {
 	char *name;
@@ -23,4 +23,24 @@ void init()
 	n = 0;
 }
 
+void load(char *fileName)
+{
+	char buffer[BUF_SIZE];
+	char *name, *number, *email, *group;
+	char *token;
 
+	FILE *fp = fopen(fileName, "r");
+	if (fp == 0)
+	{
+		printf("Open failed.\n");
+		return (0);
+	}
+	while (1)
+	{
+		if (read_line(fp, buffer, BUF_LENGTH) <= 0)
+			break ;
+		name = strtok(buffer, "#");
+		add(strdup(name), number, email, group);
+		fclose(fp);
+	}
+}
